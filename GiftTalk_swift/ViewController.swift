a54fb0fa58275d68527bf747a620d4fe4e219210
cckv <opencom@cckv-mac-mini.local>
2016-05-03 10:48:44 +0800

    Initial Commit

---
+++ GiftTalk_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiftTalk_swift
+//
+//  Created by OpenCom on 16/5/3.
+//  Copyright © 2016年 cckv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
